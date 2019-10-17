a6e731b93f5ccb856119f5168279c907d11af7cf
vkirako <vkirakosyan@sdvor.com>
2019-10-17 06:46:19 +0300

    Верстка фильтров

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
-window.onload = function() {
+const store = {
+  products: undefined,
 
-  const productRender = () => {
+  getProducts() {
     fetch('http://www.mocky.io/v2/5d944b9f2f00006b008ff619')
       .then(response => {
-        if(response.ok) {
+        if (response.ok) {
           return response.json();
         }
       })
       .then(
         data => {
-          const products = data.data.map(item => {
-            return(
-              `
-            <div class="product">
-              <img class="img" src="${item.image}" />
-              <div class="infoBox">
-                <p class="title">${item.title}</p>
-                <p class="category">${item.category.title !== undefined ? item.category.title : ''}</p>
-                <p class="price">Цена: ${item.price}</p>
-              </div>
-            </div>
-            `
-            );
-          });
-          document.getElementById('productsBox').innerHTML = products.join('');
+          this.products = data.data;
+          productRender();
         }
       );
-  };
-
-  productRender();
+  }
 };
+
+const productRender = () => {
+  if(store.products !== undefined) {
+    const listing = store.products.map(item => {
+        return (
+          `<div class="product">
+           <img class="img" src="${item.image}" />
+           <div class="infoBox">
+             <p class="title">${item.title}</p>
+             <p class="category">${item.category.title !== undefined ? item.category.title : ''}</p>
+             <p class="price">Цена: ${item.price}</p>
+           </div>
+         </div>`
+        );
+      });
+    document.getElementById('productsBox').innerHTML = listing.join('');
+  }
+};
+
+store.getProducts();
