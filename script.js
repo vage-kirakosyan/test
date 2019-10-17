2ec827fb6d4dd50ef222b7a4fb4b017fb30d5889
vage-kirakosyan <fdl.mail1@gmail.com>
2019-10-17 23:06:00 +0300

    onChange для фильтров

--- script.js
+++ script.js
@@ -11,15 +11,21 @@ const store = {
       .then(
         data => {
           this.products = data.data;
-          productRender();
+          productRender(data.data);
+          console.log(store.products);
         }
       );
-  }
+  },
 };
 
-const productRender = () => {
-  if(store.products !== undefined) {
-    const listing = store.products.map(item => {
+const state = {
+  category: 1,
+  price: 0,
+  quantity: false,
+};
+
+const productRender = (productArr) => {
+    const listing = productArr.map(item => {
         return (
           `<div class="product">
            <img class="img" src="${item.image}" />
@@ -32,7 +38,66 @@ const productRender = () => {
         );
       });
     document.getElementById('productsBox').innerHTML = listing.join('');
+};
+
+const onChangeCategoryHandler = (e) => {
+  const value = e.target.value;
+  productRender(setCategoryFilter(state.category, store.products));
+  console.log(value);
+};
+
+const onChangePriceHandler = (e) => {
+  const value = e.target.value;
+  productRender(setPriceFilter(state.price, store.products));
+  console.log(value);
+};
+
+const onChangeQuantityHandler = (e) => {
+  state.quantity = e.target.checked;
+  productRender(setQuantityFilter(state.quantity, store.products));
+};
+
+const setCategoryFilter = (state, products) => {
+  if(state === 1) {
+    return products.filter(prod => prod.category.id !== 1);
+  } else if(state === 2) {
+    return products.filter(prod => prod.category.id !== 2);
+  } else if(state === 3) {
+    return products.filter(prod => prod.category.id !== 3);
+  } else if(state === 4) {
+    return products.filter(prod => prod.category.id !== 4);
+  }
+};
+const setPriceFilter = (state, products) => {
+  if(state === 1) {
+    return products.filter(prod => prod.price > 5000);
+  } else if(state === 2) {
+    return products.filter(prod => prod.price <= 5000);
+  }
+};
+const setQuantityFilter = (state, products) => {
+  if(state === true) {
+    return products.filter(prod => prod.quantity > 0);
   }
 };
 
-store.getProducts();
+const applyFilter = () => {
+
+};
+
+window.onload = function() {
+  store.getProducts();
+
+  document
+    .getElementById('category')
+    .addEventListener('change', onChangeCategoryHandler);
+
+  const priceBtns = document.getElementsByClassName('priceBtn');
+  for (let btn of priceBtns) {
+    btn.addEventListener('change', onChangePriceHandler)
+  }
+
+  document
+    .getElementById('quantity')
+    .addEventListener('change', onChangeQuantityHandler);
+};
