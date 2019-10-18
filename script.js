87df31a89040196586fe0229fcd974235b785988
Киракосян Ваге <vkirakosyan@sdvor.local>
2019-10-18 22:44:25 +0300

    Теперь сохраняется состояние фильтров при ручном вводе параметров в урл

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Хранилище листинга полученного с API, дабы при каждом чихе не бегать на сервер*/
 const store = {
   products: undefined,
 
@@ -12,61 +13,48 @@ const store = {
         data => {
           this.products = data.data;
           productRender(data.data, createFilterSettings());
+          setCheckedFilter();
           console.log(this.products);
         }
       );
   },
 };
 
+/* Тут рендерим листинг с товарами */
 const productRender = (productArr, settings) => {
-  if(settings === false) {
-    const listing = productArr.map(item => {
-      return (
-        `<div class="product">
-           <img class="img" src="${item.image}" />
-           <div class="infoBox">
-             <p class="title">${item.title}</p>
-             <p class="category">${item.category.title !== undefined ? item.category.title : ''}</p>
-             <p class="price">Цена: ${item.price}</p>
-           </div>
-         </div>`
-      );
+  const filteredListing = productArr
+    .filter(prod => {
+      if (settings.category === '1'
+        || settings.category === '2'
+        || settings.category === '3'
+        || settings.category === '4'
+      ) {
+        return String(prod.category.id) === settings.category;
+      } else if (settings.category === undefined || settings.category === '0') {
+        return true
+      }
+    })
+    .filter(prod => {
+      if (settings.price === '1') {
+        return prod.price < 5000;
+      } else if (settings.price === '2') {
+        return prod.price >= 5000;
+      } else {
+        return true;
+      }
+    })
+    .filter(prod => {
+      if (settings.quantity === 'true') {
+        return prod.quantity > 0;
+      } else if (settings.quantity === 'false' || settings.quantity === undefined) {
+        return true;
+      }
     });
-    document.getElementById('productsBox').innerHTML = listing.join('');
+  if (filteredListing.length === 0) {
+    document.getElementById('productsBox').innerHTML = '<p class="listingError">По данному запросу товаров нет</p>';
   } else {
-    const filteredListing = productArr
-      .filter(prod => {
-        if(settings.category === '1'
-          || settings.category === '2'
-          || settings.category === '3'
-          || settings.category === '4'
-        ) {
-          return String(prod.category.id) === settings.category;
-        } else if(settings.category === undefined || settings.category === '0') {
-          return true
-        }
-      })
-      .filter(prod => {
-        if(settings.price === '1') {
-          return prod.price < 5000;
-        } else if(settings.price === '2') {
-          return prod.price >= 5000;
-        } else {
-          return true;
-        }
-      })
-      .filter(prod => {
-        if(settings.quantity === 'true') {
-          return prod.quantity > 0;
-        } else if(settings.quantity === 'false' || settings.quantity === undefined) {
-          return true;
-        }
-      });
-    if(filteredListing.length === 0) {
-      document.getElementById('productsBox').innerHTML = '<p class="listingError">По данному запросу товаров нет</p>';
-    } else  {
-      const listing = filteredListing.map(item => (
-        `<div class="product">
+    const listing = filteredListing.map(item => (
+      `<div class="product">
            <img class="img" src="${item.image}" />
            <div class="infoBox">
              <p class="title">${item.title}</p>
@@ -74,31 +62,34 @@ const productRender = (productArr, settings) => {
              <p class="price">Цена: ${item.price}</p>
            </div>
          </div>`
-      ));
-      document.getElementById('productsBox').innerHTML = listing.join('');
-    }
+    ));
+    document.getElementById('productsBox').innerHTML = listing.join('');
   }
-
 };
 
+/* Обрабатываем выбор категории */
 const onChangeCategoryHandler = (e) => {
   const value = e.target.value;
   applyFilter('category', value);
   productRender(store.products, createFilterSettings());
 };
 
+/* Обрабатываем нажатие на фильтр цены */
 const onChangePriceHandler = (e) => {
   const value = e.target.value;
   applyFilter('price', value);
   productRender(store.products, createFilterSettings());
+  console.log(e);
 };
 
+/* Обрабатываем нажатие на "Товары в наличии" */
 const onChangeQuantityHandler = (e) => {
   const value = e.target.checked;
   applyFilter('quantity', value);
   productRender(store.products, createFilterSettings());
 };
 
+/* Добавляем в урл параметры */
 const applyFilter = (name, val) => {
   let newSearch ='';
   const parameters = getFilterParams();
@@ -109,9 +100,10 @@ const applyFilter = (name, val) => {
     }
   }
   newSearch += `${name}=${val}`;
-  history.pushState({}, 'lol', `?${newSearch}`);
+  history.pushState({}, 'applyFilter', `?${newSearch}`);
 };
 
+/* Берем параметры из урла */
 const getFilterParams = () => {
   return document.location.search
     .slice(1)
@@ -119,6 +111,7 @@ const getFilterParams = () => {
     .map(item => item.split('='));
 };
 
+/* Тут настройки фильтрации для функции рендера */
 const createFilterSettings = () => {
   const parameters = getFilterParams();
   const filterSettings = {
@@ -138,6 +131,35 @@ const createFilterSettings = () => {
   return filterSettings;
 };
 
+/* Стираем все параметры фильтров из урла и сбрасываем выделение с фильтров */
+const onClickClrButtonHandler = () => {
+  history.replaceState({}, 'ClrButton', 'index.html?clr=true');
+  productRender(store.products, createFilterSettings());
+  document.getElementById('category').value = '0';
+  const priceBtns = document.getElementsByClassName('priceBtn');
+  for (let btn of priceBtns) {
+    btn.checked = false;
+  }
+  document.getElementById('quantity').checked = false;
+};
+
+/* Ставим фильтры при ручном вводе параметров в урл */
+const setCheckedFilter = () => {
+  const filtersSetting = createFilterSettings();
+  if(filtersSetting.category!==undefined) {
+    document.getElementById('category').value = filtersSetting.category;
+  }
+  const priceBtns = document.getElementsByClassName('priceBtn');
+  for (let btn of priceBtns) {
+    if(btn.value === filtersSetting.price) {
+      btn.checked = true;
+    }
+  }
+  if(filtersSetting.quantity === 'true') {
+    document.getElementById('quantity').checked = true;
+  }
+};
+
 window.onload = function() {
   store.getProducts();
 
@@ -153,4 +175,8 @@ window.onload = function() {
   document
     .getElementById('quantity')
     .addEventListener('change', onChangeQuantityHandler);
+
+  document
+    .getElementById('clrButton')
+    .addEventListener('click', onClickClrButtonHandler)
 };
